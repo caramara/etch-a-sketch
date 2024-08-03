a320e361eb47563200cddbe4dbee35805c21c25a
caramara <174440721+caramara@users.noreply.github.com>
2024-08-03 19:00:05 +0300

    Add hover effect, resize button and reset button

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const container = document.querySelector('.container');
+const gridBtn = document.querySelector('#grid-btn');
+const resetBtn = document.querySelector('#reset-btn');
+const boxes = document.querySelectorAll('.box');
+createGrid(16);
 
-for(let i = 0 ; i<16*16;i++){
-    const box = document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
+function createGrid(gridSize){
+    /*boxes.forEach(e => {
+        e.classList.remove('hover');
+        e.remove();
+    });*/
+    container.innerHTML = '';
+    for(let i = 0 ; i<gridSize*gridSize;i++){
+        const box = document.createElement('div');
+        box.classList.add('box');
+        //Divide size into equal grid size pieces ex. 100%/16 = 6.25%
+        box.style.width=`${100/gridSize}%`;
+        box.style.height=`${100/gridSize}%`;
+        container.appendChild(box);
+    }
 }
+
+container.addEventListener('mouseover',(e)=>{
+    const target = e.target;
+    target.classList.add('hover');
+});
+
+gridBtn.addEventListener('click',()=>{
+    const newSize = parseInt(prompt("Enter grid size (Example: 16 for 16x16 grid):"));
+
+    if(isNaN(newSize) || newSize<0){
+        alert('Invalid number');
+        return;
+    }
+    createGrid(newSize);
+});
+
+resetBtn.addEventListener('click', () => {
+    createGrid(16);
+});
