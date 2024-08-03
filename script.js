124835cc6b9409c20ea0bf9a0bda6fc3f3da2345
caramara <174440721+caramara@users.noreply.github.com>
2024-08-03 19:29:11 +0300

    Add Rainbow toggle

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('.container');
 const gridBtn = document.querySelector('#grid-btn');
 const resetBtn = document.querySelector('#reset-btn');
-const boxes = document.querySelectorAll('.box');
+const rainbowBtn = document.querySelector('#rainbow-btn');
+let rainbowColorsToggle = false;
+//const boxes = document.querySelectorAll('.box');
 createGrid(16);
 
 function createGrid(gridSize){
@@ -22,13 +24,19 @@ function createGrid(gridSize){
 
 container.addEventListener('mouseover',(e)=>{
     const target = e.target;
-    target.classList.add('hover');
+    
+    if(rainbowColorsToggle){
+        target.style.backgroundColor = getRandomColor();
+    }else{
+        //target.classList.add('hover');
+        target.style.backgroundColor = 'green';
+    }
 });
 
 gridBtn.addEventListener('click',()=>{
     const newSize = parseInt(prompt("Enter grid size (Example: 16 for 16x16 grid):"));
 
-    if(isNaN(newSize) || newSize<0){
+    if(isNaN(newSize) || newSize<0 || newSize>100){
         alert('Invalid number');
         return;
     }
@@ -36,5 +44,25 @@ gridBtn.addEventListener('click',()=>{
 });
 
 resetBtn.addEventListener('click', () => {
+    rainbowColorsToggle = false;
     createGrid(16);
 });
+rainbowBtn.addEventListener('click',() => {
+    rainbowColorsToggle = true;
+})
+
+function getRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i =0; i<6;i++){
+        /*
+        Math.random() generates a random floating point number between 0...1
+        By multiplying the random number by 16 we scale the range from 0...1 to a range of 0...16
+        Math.floor rounds down the value to 0...15 so a total of 16 random choices
+        By using the random integer as an index for letters(length=16) we select a random character
+        The color string contains 6 characters and we assign a random one each time from 16 choices
+         */
+        color+=letters[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
